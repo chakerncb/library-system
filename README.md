252f4113cd568b962cbd6bd7456894beddebab9c
Chaker Necibi <154261868+chakerncb@users.noreply.github.com>
2024-10-25 18:57:01 +0100

    add authors and show them in

---
+++ README.md
@@ -0,0 +1,5 @@
+# Library system
+
+## Description
+
+  - This is a library system with javaScript that allows users to borrow and return books. It also allows the librarian to add and remove books and authors from the library.
--- assets/js/script.js
+++ assets/js/script.js
@@ -50,11 +50,44 @@ class book {
 }
 
 
+const authors = [];
+
+
+function submitauthor() {
+   const name = document.getElementById('Fname').value;
+    const lname = document.getElementById('Lname').value;
+    const email = document.getElementById('email').value;
+    const age = document.getElementById('age').value;
+    const country = document.getElementById('country').value;
 
 
 
-const author1 = new author('chaker', 'necibi', 'chaker@gmail.com', 19, 'algeria');
+    const author1 = new author(name, lname, email, age, country);
+    authors.push(author1);
+    showauthor();
 
-const book1 = new book('book1', author1.name, 20, 4.5);
+}
 
-console.log(book1.getSummary());
+
+// show the authors as a table in the html
+function showauthor() {
+    let tbody = document.getElementById('authorsTable');
+    let data = '';
+    authors.forEach((authors) => {
+        data += `<tr class="bg-gray-2 text-left dark:bg-meta-4" >
+                    <td class="min-w-[150px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11" >${authors.name}</td>
+                    <td class="min-w-[150px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11" >${authors.lname}</td>
+                    <td class="min-w-[150px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11" >${authors.age}</td>
+                    <td class="px-4 py-4 font-medium text-black dark:text-white">${authors.country}</td>
+                    <td min-w-[220px] px-4 py-4 font-medium text-black dark:text-white >${authors.email}</td>
+
+                </tr>`;
+    });
+    tbody.innerHTML = data;
+
+}
+
+showauthor();
+
+
+
